53dec95e857d9a59d731d2c7187630c5c7e36f3b
Dr. Raed <drraed.jb@gmail.com>
2023-09-17 00:25:34 +0200

    FutureBuilder

---
+++ lib/ui/componentsusedinfuturebuilder.dart
@@ -0,0 +1,48 @@
+import 'package:beispielfuturebuilder/Model/comment.dart';
+import 'package:flutter/material.dart';
+
+Widget displayCircularProgressIndicator(String textfordisplaying){
+  return  Center(
+    child: Container(
+      color: Colors.yellow,
+      child: Column(
+        children: [
+          Text(textfordisplaying),
+          const CircularProgressIndicator(
+            color: Colors.blue,           
+          ),
+        ],
+      )
+      )
+      );
+}
+
+Widget displayErrorMessage(String message){
+  return Center(
+    child: Container(
+      color: Colors.red,
+      child: Text(message),
+    ),
+  );
+}
+
+Widget displayCommentsInListView(List<Comment> commentsList)=>
+ListView.builder(
+  itemCount: commentsList.length,
+  itemBuilder:(context, index) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      color: Colors.pink.shade100,
+      child: Column(
+        children: [
+        Text(commentsList[index].id.toString(),
+        style:const TextStyle(color: Colors.blue,fontSize: 20)),
+        Text(commentsList[index].name),
+        Text(commentsList[index].email),
+        Text(commentsList[index].body),
+      ]),
+    );
+  }
+);
+  
--- lib/ui/kommentareseite.dart
+++ lib/ui/kommentareseite.dart
@@ -1,3 +1,5 @@
+import 'package:beispielfuturebuilder/fetchingdata/repositoryforcomments.dart';
+import 'package:beispielfuturebuilder/ui/componentsusedinfuturebuilder.dart';
 import 'package:flutter/material.dart';
 
 class KommentareSeite extends StatefulWidget {
@@ -10,6 +12,31 @@ class KommentareSeite extends StatefulWidget {
 class _KommentareSeiteState extends State<KommentareSeite> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("All comments"),
+        backgroundColor: Colors.pink,
+      ),
+      body:Container(
+        color:Colors.yellow,
+        child: FutureBuilder(
+          future: FetchingComments.fetchAllComments(), 
+          builder: ((context, snapshot) {
+            if(snapshot.hasError)
+            {
+              return displayErrorMessage("fail to fetch all comments");
+            }
+            else if(snapshot.hasData)
+            {
+              final  comments=snapshot.data;
+              return displayCommentsInListView(comments!);
+            }
+      
+            return displayCircularProgressIndicator("Warten Sie bitte");
+          }
+          )
+          ),
+      ) ,
+    );
   }
 }
